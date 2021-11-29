fe1507a71c200e886eb9fced9b866ca8cc721540
up201905405 <up201905405@fe.up.pt>
2021-11-29 14:49:35 +0000

    eliminated 4 case conditions

--- PRBS_test/PRBS_test.ino
+++ PRBS_test/PRBS_test.ino
@@ -1,116 +1,79 @@
 byte state = 0b10000001;
-byte seq [255];
+byte seq[255];
 byte output[1000];
-int k =0;
-int k2=0;
+int k = 0;
+int k2 = 0;
 unsigned int cnt = 0;
 
-
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(3, OUTPUT);
 
- for (int i = 0; i < 254; i  = i+1) {
+  for (int i = 0; i < 254; i = i + 1)
+  {
     //Serial.println(state & 1);
-    seq[i] = state&1;
-    byte newbit = (state^(state>>1))&1;
-    state = (state>>1)| (newbit<<7);
-}
-while (k<255){
-      if (seq[k] == 0 && seq[k+1] == 0){
-      
+    seq[i] = state & 1;
+    byte newbit = (state ^ (state >> 1)) & 1;
+    state = (state >> 1) | (newbit << 7);
+  }
+  while (k < 255)
+  {
+    for (int j=1; j < (seq[k+1] + seq[k]*2 + 1); j++){
+      output[k2+j] = 0;
+    }
+    output[k2] = 1;
+    k = k + 2;
+    k2 = k2 + 2 + seq[k+1] + seq[k]*2;
+  }
+  for (int t = 0; t < 80; t = t + 1)
+  {
+    output[k2] = 0;
+    k2 = k2 + 1;
+  }
+  for (int h = 0; h < k2; h = h + 1)
+  {
+    //Serial.println(k2);
+    Serial.print(output[h]);
+  }
 
-      output[k2]=1;
-      output[k2+1]=0;
-      k = k+2;
-      k2=k2+2;
-      }
-       
-        
-      
-      else if (seq[k] == 0 && seq[k+1] == 1){
-      
+  cli(); //stop interrupts
 
-        output[k2]=1;
-        output[k2+1]=0;
-        output[k2+2]=0;
-      
-       k = k+2;
-       k2=k2+3;
-        
-      }
-      else if (seq[k] == 1 && seq[k+1] == 0){
-      
-
-        output[k2]=1;
-        output[k2+1]=0;
-        output[k2+2]=0;
-        output[k2+3]=0;
-       k = k+2;
-       k2=k2+4; 
-      }
-      else {
-
-        output[k2] = 1;
-        output[k2+1]=0;
-        output[k2+2]=0;
-        output[k2+3]=0;
-        output[k2+4]=0;
-         k = k+2;
-         k2=k2+5;
-         
-        
-      }
-        }
-        for (int t = 0; t <80;t=t+1)
-        {
-          output[k2] = 0;
-          k2=k2+1;
-        }
-        for (int h =0; h<k2;h=h+1)  {
-          //Serial.println(k2);
-          Serial.print(output[h]);
-        }
-          
-
-    cli();//stop interrupts
-
-//set timer1 interrupt at 50Hz
-  TCCR1A = 0;// set entire TCCR1A register to 0
-  TCCR1B = 0;// same for TCCR1B
-  TCNT1  = 0;//initialize counter value to 0
+  //set timer1 interrupt at 50Hz
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
+  TCNT1 = 0;  //initialize counter value to 0
   // set compare match register for 50hz increments
-  OCR1A = 39999;// = (16*10^6) / (50*8) - 1 (must be <65536)
+  OCR1A = 39999; // = (16*10^6) / (50*8) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
-  TCCR1B |= (1 << CS11); // Set CS#1 bit for 8 prescaler for timer 1  
+  TCCR1B |= (1 << CS11); // Set CS#1 bit for 8 prescaler for timer 1
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
-sei();//allow interrupts
-
+  sei(); //allow interrupts
 }
 
+ISR(TIMER1_COMPA_vect)
+{ //timer1 interrupt 200Hz toggles
 
-ISR(TIMER1_COMPA_vect){//timer1 interrupt 200Hz toggles
-        
-        if (output[cnt]==1){
-            digitalWrite(3,HIGH);
-        }
-  
-         else{  
-            digitalWrite(3,LOW);
-}
-cnt = cnt+1;
-if(cnt == k2)
-{
-  cnt=0;
+  if (output[cnt] == 1)
+  {
+    digitalWrite(3, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(3, LOW);
+  }
+  cnt = cnt + 1;
+  if (cnt == k2)
+  {
+    cnt = 0;
   }
 }
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-    
-
 }
